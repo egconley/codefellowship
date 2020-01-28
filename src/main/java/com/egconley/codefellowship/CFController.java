7839f08a52f8e7d98a1eeb9d0331c5a251eb6403
egconley <31393715+egconley@users.noreply.github.com>
2020-01-28 12:48:08 -0800

    saving user info to database

--- src/main/java/com/egconley/codefellowship/CFController.java
+++ src/main/java/com/egconley/codefellowship/CFController.java
@@ -1,10 +1,18 @@
 package com.egconley.codefellowship;
 
+import com.egconley.codefellowship.models.AppUser;
+import com.egconley.codefellowship.models.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class CFController {
+
+    @Autowired
+    AppUserRepository appUserRepository;
 
     @GetMapping("/")
     public String getHome() {
@@ -14,5 +22,12 @@ public class CFController {
     @GetMapping("/signup")
     public String getSignUp() {
         return "signup";
+    }
+
+    @PostMapping("/signup")
+    public RedirectView signup(String username, String password, String email, String firstName, String lastName, String dateOfBirth, String bio) {
+        AppUser newUser = new AppUser(username, password, email, firstName, lastName, dateOfBirth, bio);
+        appUserRepository.save(newUser);
+        return new RedirectView("/");
     }
 }
