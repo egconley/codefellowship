c49b7d3e7782b46903d8d1af3241263cdab8c81e
egconley <31393715+egconley@users.noreply.github.com>
2020-01-28 10:12:55 -0800

    initial commit from Spring init

---
+++ src/test/java/com/egconley/codefellowship/CodefellowshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egconley.codefellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodefellowshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
