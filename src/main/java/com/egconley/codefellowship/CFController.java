49ed4ce9d3d154b20865b4111d1709ca02e390dd
egconley <31393715+egconley@users.noreply.github.com>
2020-01-28 11:01:54 -0800

    comments/tabbing in security config file, added @GetMapping for homepage to controller

--- src/main/java/com/egconley/codefellowship/CFController.java
+++ src/main/java/com/egconley/codefellowship/CFController.java
@@ -1,9 +1,12 @@
 package com.egconley.codefellowship;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CFController {
+
+    @GetMapping("/")
     public String getHome() {
         return "home";
     }
